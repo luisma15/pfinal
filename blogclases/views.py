d02f8b328ef748deb4fa47bb8019e950e9cf626c
luisma15 <luisma15_banda@hotmail.com>
2016-11-16 16:30:09 -0600

    Primera pagina html

--- blogclases/views.py
+++ blogclases/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Alumno, Catedratico, Curso, Asignacion
 
 # Create your views here.
+def listar_asignaciones(request):
+    #posts = Asignacion.objects.filter(published_date__lte=timezone.now()).order_by('fecha')
+    posts = Asignacion.objects.order_by('as_fecha')
+    return render(request, 'blogclases/listar_asignaciones.html', {'posts': posts})
